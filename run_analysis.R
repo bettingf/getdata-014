935ce04a8421782efc7e0f8d59e455954de8dc81
franck <Franck Bettinger>
2015-05-24 18:26:43 +0200

    start of the analysis script

---
+++ run_analysis.R
@@ -0,0 +1,10 @@
+body_acc_x_test<-read.csv("UCI HAR Dataset/test/Inertial Signals/body_acc_x_test.txt", sep="", header = FALSE, strip.white=TRUE)
+body_acc_y_test<-read.csv("UCI HAR Dataset/test/Inertial Signals/body_acc_y_test.txt", sep="", header = FALSE, strip.white=TRUE)
+body_acc_z_test<-read.csv("UCI HAR Dataset/test/Inertial Signals/body_acc_z_test.txt", sep="", header = FALSE, strip.white=TRUE)
+body_gyro_x_test<-read.csv("UCI HAR Dataset/test/Inertial Signals/body_gyro_x_test.txt", sep="", header = FALSE, strip.white=TRUE)
+body_gyro_y_test<-read.csv("UCI HAR Dataset/test/Inertial Signals/body_gyro_y_test.txt", sep="", header = FALSE, strip.white=TRUE)
+body_gyro_z_test<-read.csv("UCI HAR Dataset/test/Inertial Signals/body_gyro_z_test.txt", sep="", header = FALSE, strip.white=TRUE)
+total_acc_x_test<-read.csv("UCI HAR Dataset/test/Inertial Signals/total_acc_x_test.txt", sep="", header = FALSE, strip.white=TRUE)
+total_acc_y_test<-read.csv("UCI HAR Dataset/test/Inertial Signals/total_acc_y_test.txt", sep="", header = FALSE, strip.white=TRUE)
+total_acc_z_test<-read.csv("UCI HAR Dataset/test/Inertial Signals/total_acc_z_test.txt", sep="", header = FALSE, strip.white=TRUE)
+
